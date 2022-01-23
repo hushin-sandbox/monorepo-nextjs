a501e133a3d132b58d918013daedb2cd946722dd
hushin <ngtv.hsn@gmail.com>
2022-01-23 18:38:48 +0900

    add @typescript-eslint/eslint-plugin

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -8,4 +8,12 @@ module.exports = {
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
   },
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    },
+  ],
 };
